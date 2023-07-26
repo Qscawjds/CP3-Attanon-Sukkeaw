c2784c787a098bab72f5ec6002b96732c3333ae6
Qscawjds <sukkeawattanon@gmail.com>
2023-07-26 23:21:47 +0700

    Create Exercise4_Attanon_S.py

---
+++ Exercise4_Attanon_S.py
@@ -0,0 +1,9 @@
+FE = 100.0 #Foundation English
+GB = 98.5 #General Business
+ICS = 80.5 #Introduction to Computer Systems
+CP = 78.5 #Computer Programming
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
