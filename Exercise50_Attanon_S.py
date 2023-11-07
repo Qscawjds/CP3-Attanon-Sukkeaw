6156887131021f569b9022d2f9fc126470360550
Qscawjds <sukkeawattanon@gmail.com>
2023-11-07 16:03:52 +0700

    Delete Exercise50_Attanon_S.py

--- Exercise50_Attanon_S.py
+++
@@ -1,6 +0,0 @@
-def Math(x,y):
-    print(str(x),"+",str(y),"=",x+y)
-    print(str(x),"-",str(y),"=",x-y)
-    print(str(x),"*",str(y),"=",x*y)
-    print(str(x),"/",str(y),"=",x/y)
-Math(x = int(input("Enter Number :")),y = int(input("Enter Number :")))
